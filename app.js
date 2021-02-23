030b22f41c55a14abf84ff899a2e70f6ccec84fd
basmaalqadiri <basmanaserqadiri@gmail.com>
2021-02-23 23:51:45 +0200

    added java script

---
+++ app.js
@@ -0,0 +1,21 @@
+var age=25;
+var name="Ahmad"
+var isPassed=true;
+var isAbsent="false";
+var userName="Khaled";
+
+console.log(age);
+console.log("hello everyone");
+console.log(55);
+
+if (age<18){
+    console.log("test1");
+}
+
+if (age>19){
+    console.log("test2");
+}
+    var nameuserinput=prompt("try to guess the name?");
+var ageuserinput=prompt("guess the age");
+var coloruserinput=prompt("what do you think is my fav color?")
+
